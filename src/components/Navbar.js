eb6e45b751fd88cce0960c9dfbcb0c65dab70d8d
Sai Purciful <saipurciful@gmail.com>
2021-04-09 00:43:40 -0700

    added Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Sai from "../sai.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#"><img className="logo" src={Sai} alt="Sai's photo" /></a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <FontAwesomeIcon icon={faBars} style={{ color: "#ffff" }} />
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Resume</a>
+                        </li>
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
