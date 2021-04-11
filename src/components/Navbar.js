51c38f98aeb962ee4f5c0df60094987f6e26e7de
Sai Purciful <saipurciful@gmail.com>
2021-04-10 19:49:05 -0700

    installed gh-pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faHome } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-scroll"
 
 
